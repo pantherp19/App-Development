c70f6ff33319cea36039e79379dcc6f49123c793
pantherp19 <pantherp861@gmail.com>
2024-08-30 15:17:14 +0530

    updated package

---
+++ AppPro.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+export default function AppPro(): JSX.Element {
+  const isDark = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={isDark ? styles.whiteText : styles.darkText}>
+          Hello World
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  whiteText: {
+    color: 'white',
+  },
+
+  darkText: {
+    color: 'black',
+  },
+});
