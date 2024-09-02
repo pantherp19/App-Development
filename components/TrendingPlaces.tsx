659b457c3a2367c9895d3b321a1cb56ffa9ebe59
pantherp19 <pantherp861@gmail.com>
2024-09-02 12:29:34 +0530

    image card

--- components/TrendingPlaces.tsx
+++ components/TrendingPlaces.tsx
@@ -6,34 +6,14 @@ export default function TrendingPlaces() {
     <View>
       <Text style={styles.headingText}>Trending Places</Text>
       <ScrollView style={styles.container}>
-        <View style={styles.box}>
+        <View style={styles.card}>
           <Image
             source={{
               uri: 'https://images.pexels.com/photos/14455613/pexels-photo-14455613.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
             }}
             style={styles.imageCard}
           />
-          <View>
-            <Text style={styles.cardTitle}>
-              Architectural beauty - Napier Museum
-            </Text>
-            <Text style={styles.cardLabel}>Thiruvananthapuram, KL, India</Text>
-            <Text style={styles.cardDescription}>
-              Built in 1855 and designed by Robert Fellows Chisholm, it
-              showcases a unique blend of Indian, Chinese, Kerala, and Mughal
-              architectural styles.
-            </Text>
-            <Text style={styles.cardFooter}>12 mins away from you</Text>
-          </View>
-        </View>
-        <View style={styles.box}>
-          <Image
-            source={{
-              uri: 'https://images.pexels.com/photos/15928901/pexels-photo-15928901/free-photo-of-ruin-of-an-ancient-building-with-bass-reliefs.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            }}
-            style={styles.imageCard}
-          />
-          <View>
+          <View style={styles.cardDetails}>
             <Text style={styles.cardTitle}>
               Architectural beauty - Napier Museum
             </Text>
@@ -62,24 +42,50 @@ const styles = StyleSheet.create({
     margin: 8,
   },
 
-  box: {
-    width: 335,
-    margin: 8,
+  card: {
+    width: 390,
+    margin: 0,
+    borderRadius: 12,
+    elevation: 4,
+    backgroundColor: '#2A2C2C',
+    padding: 10,
+    // overflow: 'hidden',
   },
 
   imageCard: {
     height: 200,
     width: 'auto',
+    margin: 6,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+
+  cardDetails: {
+    flex: 1,
+    flexGrow: 1,
+    padding: 8,
   },
 
   cardTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 6,
   },
 
-  cardLabel: {},
+  cardLabel: {
+    fontSize: 14,
+    marginBottom: 6,
+  },
 
-  cardDescription: {},
+  cardDescription: {
+    fontSize: 12,
+    marginBottom: 6,
+    color: '#7B8788',
+  },
 
-  cardFooter: {},
+  cardFooter: {
+    color: '#5da3fa',
+  },
 });
